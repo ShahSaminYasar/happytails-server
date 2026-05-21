59f702599585a95c0af4dfc7a5d273b33f81e20a
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2026-05-21 22:07:25 +0600

    Added: Pets GET route

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://ssy-happytails.vercel.app"],
+    credentials: true,
+  }),
+);
 
 dotenv.config();
 
@@ -22,9 +28,48 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 const db = client.db("happyTails");
 const petsCollection = db.collection("pets");
 
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
+);
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
 async function run() {
   try {
     await client.connect();
+
+    app.get("/pets", async (req, res) => {
+      const { name, species, ownerEmail, limit } = req.query;
+      const query = {};
+
+      if (name) query.name = { $regex: name, $options: "i" };
+      if (species) query.species = { $in: species.split(",") };
+      if (ownerEmail) query.ownerEmail = ownerEmail;
+
+      const pets = await petsCollection
+        .find(query)
+        .limit(limit ? parseInt(limit) : 0)
+        .toArray();
+      res.json(pets);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
